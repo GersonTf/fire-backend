55309e679ac9fc1cfc74d52d41b793a423cd36d2
gerson <tnii.gerson@gmail.com>
2025-07-09 21:14:39 +0200

    added login endpoint for test

--- api/server.go
+++ api/server.go
@@ -4,34 +4,38 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/GersonTf/fire-backend/storage"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type Server struct {
 	listenAddr string
 	store      storage.Storer
+	jwtSecret  []byte
 }
 
 func NewServer(listenAddr string, store storage.Storer) *Server {
 	return &Server{
 		listenAddr: listenAddr,
 		store:      store,
+		jwtSecret:  []byte("your-secret-key"), // TODO: move to config/env
 	}
 }
 
 func (s *Server) Start() error {
-	http.HandleFunc("/user", s.handleGetUserByID)
-	http.HandleFunc("/health", s.handleHealthCheck)
+	http.HandleFunc("GET /user/{id}", s.handleGetUserByID)
+	http.HandleFunc("GET /health", s.handleHealthCheck)
+	http.HandleFunc("POST /login", s.handleLogin)
 
 	return http.ListenAndServe(s.listenAddr, nil)
 }
 
 func (s *Server) handleGetUserByID(w http.ResponseWriter, r *http.Request) {
-	//todo errors? aldo get id from request
-	userID := r.URL.Query().Get("id")
+	userID := r.PathValue("id")
 	if userID == "" {
-		http.Error(w, "Missing 'id' query parameter", http.StatusBadRequest)
+		http.Error(w, "Missing user ID in path", http.StatusBadRequest)
 		return
 	}
 
@@ -53,6 +57,34 @@ func (s *Server) handleGetUserByID(w http.ResponseWriter, r *http.Request) {
 func (s *Server) handleHealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("I am alive!!"))
+	if err != nil {
+		panic(err) //todo temporary while I define a proper strategy for errors in the handlers
+	}
+}
+
+func (s *Server) handleLogin(w http.ResponseWriter, r *http.Request) {
+	// Create JWT token with simple claims
+	claims := jwt.MapClaims{
+		"user_id": "demo-user",                           // TODO: get from request body
+		"exp":     time.Now().Add(time.Hour * 24).Unix(), // 24 hour expiration
+		"iat":     time.Now().Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(s.jwtSecret)
+	if err != nil {
+		log.Printf("Failed to generate JWT token: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Return token as JSON
+	response := map[string]string{
+		"token": tokenString,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(response)
 	if err != nil {
 		panic(err) //todo temporary while I define a proper strategy for errors in the handlers
 	}
